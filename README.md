218dc7437cf22967693fb65646611babe8678fc9
Annamuratov Javlon <annamuratov1987@gmail.com>
2021-05-23 12:27:01 +0500

    Values

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # gobyexample
 
 [Hello World](https://github.com/annamuratov1987/gobyexample/blob/main/hello_world.go)
+[Values](https://github.com/annamuratov1987/gobyexample/blob/main/values.go)
--- values.go
+++ values.go
@@ -1,1 +1,33 @@
 package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("go"+"lang")
+	fmt.Println(`go`+`lang`)
+	fmt.Println('g')
+	fmt.Println('o')
+	fmt.Println('g'+'o')
+
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("11/3=", 11/3)
+	fmt.Println("11.0/3.0=", 11.0/3.0)
+
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+/* Result:
+golang
+golang
+103
+111
+214
+1+1= 2
+11/3= 3
+11.0/3.0= 3.6666666666666665
+true
+true
+false
+ */
